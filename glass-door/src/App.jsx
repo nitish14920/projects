a0d3b3b3c8534595e8799e34dfafeec7b5aa7194
nitish14920 <nitish14920@gmail.com>
2021-07-20 14:54:03 +0530

    gnew

---
+++ glass-door/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Gdfordev } from './Components/Gdfordev';
+
+function App() {
+  return (
+    <div className="App">
+      <Gdfordev/>
+    </div>
+  );
+}
+
+export default App;
